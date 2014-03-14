788800680df86c86069f2ec85ae8a5d4fcaccf59
Sachin <sachin@sachin-desktop.(none)>
2014-03-14 19:19:59 +0530

    setup landing page and registration

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  #validates :terms_condition, presence: {message: 'Please read and accept the official rules.'}
+
+  #validates :terms_condition, acceptance: true
+  validates :terms_of_service, acceptance: true
+
+  #validate do |user|
+  #  user.errors.add(:base, "Please read and accept the official rules.") if user.terms_condition.blank?
+  #end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Luckybudhabeer::Application.config.secret_key_base = '7af77baebb9180740352f14b3ca032b0d0f379270ec5549bd5cae1704b4ddca749410bd8179dc2a0b4ea15d174fc707a85613ef0aa6049d374dd3507ee878334'
