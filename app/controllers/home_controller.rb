ee22d2c2d9ab8468f4e23adb04dcefdf34cbc4ef
Satish <satish.zol@bacancytechnology.com>
2014-03-16 00:34:43 +0530

    adde controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :thanks]
 
   def index
-  	@teams = Teams.order("created_at ASC")
-  	@sub_array = @teams.each_slice(2).to_a
-  	respond_to do |format|
-  		format.html
-  		format.json{render json: @teams}
-  	end
+  	#@teams = Teams.order("created_at ASC")
+  	#@sub_array = @teams.each_slice(2).to_a
+  	#respond_to do |format|
+  #		format.html
+  #		format.json{render json: @teams}
+ # 	end
   end
 
 
